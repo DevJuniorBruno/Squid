8e4a45480256e309a2997ec4cd1ef837c586617e
DevJúnior Bruno <brunode2001@gmail.com>
2023-09-05 01:59:48 -0300

    update bug

--- src/components/whatsButton/page.tsx
+++ src/components/whatsButton/page.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
-import './WhatsAppButton.css';
+import './whatsAppButton.css';
 
 const WhatsAppButton = () => {
   return (
