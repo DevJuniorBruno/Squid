0264826709ee8b0d0d9693b2a0d1ca128a8d7c48
DevJuniorBruno <brunode2001@gmail.com>
2023-11-06 01:31:44 -0300

    add buttonclose

--- src/components/message/page.tsx
+++ src/components/message/page.tsx
@@ -8,11 +8,25 @@ export default function Menssage(){
 
     const [showMessage, setShowMessage] = useState(false);
 
+    const [classNone, setClassNone] = useState("")
+
     useEffect(() => {
         // Use setTimeout para atrasar a exibição do conteúdo por 5 segundos.
+
+        
+        
         const timer = setTimeout(() => {
           setShowMessage(true);
         }, 5000);
+
+        const timerReset = setTimeout(() => {
+          setShowMessage(false);
+        }, 20000);
+
+        const timerReload = setTimeout(() => {
+          setShowMessage(true);
+        }, 25000);
+        
     
         // Certifique-se de limpar o timer para evitar vazamentos de memória.
         return () => clearTimeout(timer);
@@ -21,12 +35,16 @@ export default function Menssage(){
       return (
         <div>
           {showMessage ? (
-             <div className="containerMessage">
+             <div className={`containerMessage ${classNone} `}  >
+
 
              <h1>
                  Quer que  seu negócio aumente o faturamento através da nossa estratégia ?
                  Entre em contato e tire suas duvidas, teremos o maior prazer em lhe atender!
              </h1>
+              <button className='buttonClose' onClick={()=>setClassNone("classNone")} >
+                x
+              </button>
  
          </div>
           ) : (
