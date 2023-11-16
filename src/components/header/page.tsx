c172e395548b5ca263304a1773ca273a64a77cb1
DevJuniorBruno <brunode2001@gmail.com>
2023-11-16 16:18:35 -0300

    test pixels

--- src/components/header/page.tsx
+++ src/components/header/page.tsx
@@ -33,11 +33,15 @@ export default function Header(){
 
             <script
                 dangerouslySetInnerHTML={{
-                    __html:`( window.dataLayer = window.dataLayer || [];
-                        function gtag(){dataLayer.push(arguments);}
-                        gtag('js', new Date());
-                      
-                        gtag('config', 'AW-11413970221');)`
+                    __html:`( <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11413970221">
+                    </script>
+                    <script>
+                      window.dataLayer = window.dataLayer || [];
+                      function gtag(){dataLayer.push(arguments);}
+                      gtag('js', new Date());
+                    
+                      gtag('config', 'AW-11413970221');
+                    </script>)`
                 }}
             />
 
