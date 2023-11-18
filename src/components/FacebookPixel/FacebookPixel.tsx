b22fbc2451810230f6fa4d47b1ea6bcb808667c5
DevJuniorBruno <brunode2001@gmail.com>
2023-11-18 17:17:04 -0300

    update facePixel

--- src/components/FacebookPixel/FacebookPixel.tsx
+++ src/components/FacebookPixel/FacebookPixel.tsx
@@ -9,18 +9,25 @@ const FacebookPixel = () => {
     script.innerHTML = `
    
     
-    !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-    n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
-    n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
-    t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
-    document,'script','https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '1537857630318161');
-    fbq('track', 'PageView');
-    `;
+
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1537857630318161');
+fbq('track', 'PageView');
+
+
+
+`;
+<noscript><img className='facebookNoscript'
+src="https://www.facebook.com/tr?id=1537857630318161&ev=PageView&noscript=1"
+/></noscript>
     
-    <noscript><img className='facebookNoscript'
-    src="https://www.facebook.com/tr?id=1537857630318161&ev=PageView&noscript=1"
-    /></noscript>
 
     // Adicione o script à cabeça (head) do documento
     document.head.appendChild(script);
