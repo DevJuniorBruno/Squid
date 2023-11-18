8bb0d29f37231bed6c4d04be3a88ce02e96a6500
DevJuniorBruno <brunode2001@gmail.com>
2023-11-18 15:05:49 -0300

    update pixelFace

--- src/components/FacebookPixel/FacebookPixel.tsx
+++ src/components/FacebookPixel/FacebookPixel.tsx
@@ -1,13 +1,14 @@
 // components/FacebookPixel.tsx
 import { useEffect } from 'react';
+import 'facebookPixel.css'
 
 const FacebookPixel = () => {
   useEffect(() => {
     // Crie um script element para carregar o código do Pixel do Facebook
     const script = document.createElement('script');
     script.innerHTML = `
-    <!-- Facebook Pixel Code -->
-    <script>
+   
+    
     !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
     n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
     n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
@@ -15,13 +16,11 @@ const FacebookPixel = () => {
     document,'script','https://connect.facebook.net/en_US/fbevents.js');
     fbq('init', '1537857630318161');
     fbq('track', 'PageView');
-    </script>
-    <noscript><img height="1" width="1" style="display:none"
+    `;
+    
+    <noscript><img className='facebookNoscript'
     src="https://www.facebook.com/tr?id=1537857630318161&ev=PageView&noscript=1"
     /></noscript>
-    <!-- DO NOT MODIFY -->
-    <!-- End Facebook Pixel Code -->
-    `;
 
     // Adicione o script à cabeça (head) do documento
     document.head.appendChild(script);
